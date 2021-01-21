3c63a9fd9cbfa9ed8316d35d9d9e67699db99cc3
JohnComo <John.Como@macaulay.cuny.edu>
2021-01-21 14:39:42 -0500

    backend complete

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,15 @@
 const postsResolvers = require('./posts')
-const userResolvers = require ('./users')
+const usersResolvers = require ('./users')
+const commentsResolvers = require('./comments')
 
 module.exports = { 
     Query: { 
         ...postsResolvers.Query 
     }, 
     Mutation: {
-        ...userResolvers.Mutation
+        ...usersResolvers.Mutation, 
+        ...postsResolvers.Mutation, 
+        ...commentsResolvers.Mutation
     }
 }
+
