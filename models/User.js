93163dac0dddeaf3483ed21fef4640ddb3334520
JohnComo <John.Como@macaulay.cuny.edu>
2021-01-22 02:03:16 -0500

    front end complete

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-const { model, Schema} = require('mongoose')
 
-const userSchema = new Schema({ 
-    username: String, 
-    password: String, 
-    email: String, 
-    createdAt: String
-})
+const { model, Schema } = require("mongoose");
 
-module.exports = model('User', userSchema)
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  createdAt: String,
+});
+
+module.exports = model("User", userSchema);
